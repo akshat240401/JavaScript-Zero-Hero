fd47538de522c354acb7a9d70369012b0b5a90d3
Akshat Mehta <akshat2404@gmail.com>
2022-06-22 11:36:28 +0530

    Print Function - Console API & On Screen

---
+++ index.js
@@ -0,0 +1,8 @@
+// Print on Screen
+// alert('Welcome');
+document.write("Document write is written here")
+
+// Print on Console :- JavaScript-Console API
+console.log("Hello World");
+console.warn("This is warning")
+console.error("This is error")
